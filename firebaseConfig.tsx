8bc7892ba34c94b60c7c30404e475825460f2b34
Erick <erickvini1606@gmail.com>
2025-10-22 00:40:03 -0300

    Arquivo de configuração para conexão com o banco(Firebase).

---
+++ firebaseConfig.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFj1hA8VG4ljVwEF-Q-Cmm5ucmLQ4xJrY",
+  authDomain: "projetoutfpr-7106b.firebaseapp.com",
+  projectId: "projetoutfpr-7106b",
+  storageBucket: "projetoutfpr-7106b.firebasestorage.app",
+  messagingSenderId: "598688183207",
+  appId: "1:598688183207:web:1f478e0262ed4980743c82"
+};
+
+const app = initializeApp(firebaseConfig);
